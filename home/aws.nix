ffda3e46b8575bf64523d2b8ec1b9fda8a37d05b
mfyuu <83203852+mfyuu@users.noreply.github.com>
2026-02-19 22:15:15 +0900

    refactor(home/aws): generate SSO profiles with mkSsoProfile helper
    - extract shared SSO fields into mkSsoProfile function
    - each profile now only specifies name and account ID
    - reduces repetition from 53 lines to 35 lines

--- home/aws.nix
+++ home/aws.nix
@@ -1,54 +1,35 @@
 { ... }:
-{
-  home.file.".aws/config".text = ''
-    [profile tip-extra]
-    region = ap-northeast-1
-    output = json
+let
+  mkSsoProfile = name: accountId: ''
+    [profile ${name}]
     sso_session = sso
-    sso_account_id = 341238550926
-    sso_role_name = AWSAdministratorAccess
-    [profile sandbox]
-    sso_session = sso
-    sso_account_id = 962800862825
+    sso_account_id = ${accountId}
     sso_role_name = AWSAdministratorAccess
     region = ap-northeast-1
     output = json
+  '';
+
+  ssoSession = ''
     [sso-session sso]
     sso_start_url = https://toda.awsapps.com/start
     sso_region = ap-northeast-1
     sso_registration_scopes = sso:account:access
-    [profile haisou-kanri]
-    sso_session = sso
-    sso_account_id = 036890309087
-    sso_role_name = AWSAdministratorAccess
-    region = ap-northeast-1
-    output = json
-    [profile koutei]
-    sso_session = sso
-    sso_account_id = 474668423034
-    sso_role_name = AWSAdministratorAccess
-    region = ap-northeast-1
-    output = json
-    [profile toda]
-    sso_session = sso
-    sso_account_id = 434274117771
-    sso_role_name = AWSAdministratorAccess
-    region = ap-northeast-1
-    output = json
-    [profile niyarepo]
-    sso_session = sso
-    sso_account_id = 649213662808
-    sso_role_name = AWSAdministratorAccess
-    region = ap-northeast-1
-    output = json
-    [profile booklift]
-    sso_session = sso
-    sso_account_id = 537452063997
-    sso_role_name = AWSAdministratorAccess
-    region = ap-northeast-1
-    output = json
-    [profile amplify-toda]
-    credential_process = aws configure export-credentials --profile toda
-    region = ap-northeast-1
   '';
+in
+{
+  home.file.".aws/config".text = builtins.concatStringsSep "" [
+    (mkSsoProfile "tip-extra" "341238550926")
+    (mkSsoProfile "sandbox" "962800862825")
+    ssoSession
+    (mkSsoProfile "haisou-kanri" "036890309087")
+    (mkSsoProfile "koutei" "474668423034")
+    (mkSsoProfile "toda" "434274117771")
+    (mkSsoProfile "niyarepo" "649213662808")
+    (mkSsoProfile "booklift" "537452063997")
+    ''
+      [profile amplify-toda]
+      credential_process = aws configure export-credentials --profile toda
+      region = ap-northeast-1
+    ''
+  ];
 }
