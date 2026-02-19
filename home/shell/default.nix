8f390299b16fd9fc08fd49bf30c8f64b89be7d3b
mfyuu <83203852+mfyuu@users.noreply.github.com>
2026-02-19 21:39:16 +0900

    refactor(shell): move session env config out of zsh.nix
    - move home.sessionPath and home.sessionVariables to shell/default.nix
    - these options are shell-agnostic and managed by home-manager independently
      of the zsh module, so they belong at the shell-group level
    - zsh.nix now only contains zsh-specific configuration

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -8,4 +8,17 @@
     ./eza.nix
     ./fzf.nix
   ];
+
+  home.sessionPath = [
+    "$HOME/bin"
+    "$HOME/.local/bin"
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+    GH_PAGER = "less -RFX";
+    PAGER = "less -RFX";
+    HOMEBREW_FORBIDDEN_FORMULAE = "node npm pnpm yarn bun deno go awscli bat direnv eza fastfetch fd fzf gh lefthook mise starship turbo zoxide claude ripgrep";
+    ZENO_HOME = "$HOME/.config/zeno";
+  };
 }
--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -2,19 +2,6 @@
 {
   xdg.configFile."zeno/config.yml".source = ./zeno-config.yml;
 
-  home.sessionPath = [
-    "$HOME/bin"
-    "$HOME/.local/bin"
-  ];
-
-  home.sessionVariables = {
-    EDITOR = "vim";
-    GH_PAGER = "less -RFX";
-    PAGER = "less -RFX";
-    HOMEBREW_FORBIDDEN_FORMULAE = "node npm pnpm yarn bun deno go awscli bat direnv eza fastfetch fd fzf gh lefthook mise starship turbo zoxide claude ripgrep";
-    ZENO_HOME = "$HOME/.config/zeno";
-  };
-
   programs.zsh = {
     enable = true;
     enableCompletion = true;
