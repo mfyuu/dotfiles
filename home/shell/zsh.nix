856d41f8b68b3bda487b051484c3455219520ba2
mfyuu <83203852+mfyuu@users.noreply.github.com>
2026-02-24 10:34:46 +0900

    feat(shell): add essential zsh configurations
    - add GPG_TTY export for commit signing
    - add brew FPATH for homebrew tool completions (gh, etc.)
    - add zoxide interactive aliases (cd='z', cdi='zi')
    - add shell aliases (tree, warp, ccusage, difit, biome-config)

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -6,7 +6,27 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
+    shellAliases = {
+      tree = "eza -T -a -I 'node_modules|.git|.cache' --icons";
+      warp = "open -a Warp";
+      ccusage = "bunx ccusage@latest";
+      difit = "bunx difit@latest";
+      biome-config = "bunx @mfyuu/biome-config";
+    };
+
     initContent = ''
+      # GPG_TTY (dynamic value, cannot use sessionVariables)
+      export GPG_TTY=$TTY
+
+      # brew completions (gh, etc.)
+      if type brew &>/dev/null; then
+        FPATH="$(brew --prefix)/share/zsh/site-functions:''${FPATH}"
+      fi
+
+      # zoxide interactive aliases
+      [[ -o interactive ]] && alias cd='z'
+      [[ -o interactive ]] && alias cdi='zi'
+
       # History search with arrow keys
       autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
       zle -N up-line-or-beginning-search
