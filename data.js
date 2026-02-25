540db90c1c1c373491f1a1b72ebabbdf27b2afa9
github-action-benchmark <github@users.noreply.github.com>
2026-02-25 07:43:02 +0000

    add Zsh Startup Time (customSmallerIsBetter) benchmark result for f69c702fd4a5f0395a5cc32e1decf7b381acc654

---
+++ data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1772005382563,
+  "repoUrl": "https://github.com/mfyuu/dotfiles",
+  "entries": {
+    "Zsh Startup Time": [
+      {
+        "commit": {
+          "author": {
+            "email": "83203852+mfyuu@users.noreply.github.com",
+            "name": "mfyuu",
+            "username": "mfyuu"
+          },
+          "committer": {
+            "email": "83203852+mfyuu@users.noreply.github.com",
+            "name": "mfyuu",
+            "username": "mfyuu"
+          },
+          "distinct": true,
+          "id": "f69c702fd4a5f0395a5cc32e1decf7b381acc654",
+          "message": "chore(ci): serve benchmark results from root of gh-pages",
+          "timestamp": "2026-02-25T16:40:09+09:00",
+          "tree_id": "ab05845e7367ee9816c4d0440f9854fe142f58db",
+          "url": "https://github.com/mfyuu/dotfiles/commit/f69c702fd4a5f0395a5cc32e1decf7b381acc654"
+        },
+        "date": 1772005381566,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh -i -c exit",
+            "value": 38.96,
+            "range": "6.48 ms",
+            "unit": "ms",
+            "extra": "median: 38.96 ms\nmin: 37.21 ms\nmax: 67.3 ms\nruns: 50"
+          }
+        ]
+      }
+    ]
+  }
+}
